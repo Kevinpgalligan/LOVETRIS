b96b87e234732ab0e7d4cf3ce8bca077d12cd199
kevinpgalligan <galligankevinp@gmail.com>
2020-04-05 21:39:20 +0100

    Most of the code for defining pieces & manipulating them.

--- src/hatetris.lisp
+++ src/hatetris.lisp
@@ -12,6 +12,116 @@
 (defun get-cell (state x y)
   (aref (state-well game) y x))
 
+;; Pieces have a 4x4 bounding box, x & y point
+;; to the top left corner of the bounding box.
+;; They also store the ID of the piece. Pointers
+;; to the parts of the bounding box that are filled
+;; in. And the orientation of the piece. Orientation
+;; is changed in conjunction with the pointers to the
+;; filled in parts.
+(defstruct piece x y id filled-coords orientation)
+
+(defun piece-down (piece)
+  (make-piece :x (piece-x piece)
+              :y (1+ (piece-y piece))
+              :id (piece-id piece)
+              :filled-coords (piece-filled-coords piece)
+              :orientation (piece-orientation piece)))
+
+(defun piece-left (piece)
+  (make-piece :x (1- (piece-x piece))
+              :y (piece-y piece)
+              :id (piece-id piece)
+              :filled-coords (piece-filled-coords piece)
+              :orientation (piece-orientation piece)))
+
+(defun piece-right (piece)
+  (make-piece :x (1+ (piece-x piece))
+              :y (piece-y piece)
+              :id (piece-id piece)
+              :filled-coords (piece-filled-coords piece)
+              :orientation (piece-orientation piece)))
+
+(defun piece-rotate (piece)
+  (multiple-value-bind (new-filled-coords new-orientation)
+      (next-orientation (piece-id piece) (piece-orientation))
+    (make-piece :x (piece-x piece)
+                :y (piece-y piece)
+                :id (piece-id piece)
+                :filled-coords new-filled-coords
+                :orientation new-orientation)))
+
+(defconstant piece-bbox-size 4)
+
+(defparameter piece-orientations
+  (make-hash-table :test #'equal))
+
+(defun get-orientations (piece-id)
+  (gethash piece-id piece-orientations))
+
+(defun next-orientation (piece-id orientation)
+  (let* ((orientations (get-orientations piece-id))
+         (next-orientation-index (mod (1+ orientation)
+                                      (length orientations))))
+    (values next-orientation-index
+            (nth next-orientation-index orientations))))
+
+(defun add-piece (template)
+  ;; Create & return constructor function, and
+  ;; also add the piece's orientations to the list.
+  (when (not (= +piece-bbox-size+ (length template)))
+    (error "Piece is the wrong size." template))
+  (defparameter filled-coords (list))
+  (loop for y from 0 below +piece-bbox-size+ do
+        (let ((row (nth y template)))
+          (when (not (= +piece-bbox-size+ (length row)))
+            (error "Piece row is wrong size." template))
+          (loop for x from 0 below +piece-bbox-size+ do
+                (when (equal #\# (char row x))
+                  (push (list x y) (car filled-coords))))))
+  ;; TODO do rotations (x=4-y-1, y=x), add to orientations list.
+  ;; then return a constructor.
+  ;; Also... should moving a piece always create a new struct?
+  ;; Seems a bit wasteful.
+  ;; Not sure what the next steps are after that.
+  )
+
+;; Order is important, as described in
+;; the original HATETRIS code.
+(defconstant +piece-constructors+
+  (mapcar
+   #'add-piece
+   ;; Templates.
+   (list
+    '("...."
+      "..##"
+      ".##."
+      "....")
+    '("...."
+      ".##."
+      "..##"
+      "....")
+    '("...."
+      ".##."
+      ".##."
+      "....")
+    '("...."
+      "####"
+      "...."
+      "....")
+    '("...."
+      ".###"
+      ".#.."
+      "....")
+    '("...."
+      ".##."
+      ".#.."
+      ".#..")
+    '("...."
+      ".###"
+      "..#."
+      "....")
+    )))
 ;; things to do:
 ;;   1. generate next possible moves given a certain piece.
 ;;      1.1. define pieces & their unique rotations.
