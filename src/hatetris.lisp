f8e425dcceee0b255274ba2d07c848aecca00eb9
kevinpgalligan <galligankevinp@gmail.com>
2020-04-06 01:49:57 +0100

    Finished implementing pieces, their orientations & piece moves.

--- src/hatetris.lisp
+++ src/hatetris.lisp
@@ -5,12 +5,12 @@
 
 (defstruct state well score)
 
-(defun new-state (&key (width 10) (height 16))
-  (make-state :well (make-array (list height width) :initial-contents +empty+)
+(defun new-state (&key (width 10) (height 20))
+  (make-state :well (make-array (list height width) :initial-element +empty+)
               :score 0))
 
 (defun get-cell (state x y)
-  (aref (state-well game) y x))
+  (aref (state-well state) y x))
 
 ;; Pieces have a 4x4 bounding box, x & y point
 ;; to the top left corner of the bounding box.
@@ -19,39 +19,37 @@
 ;; in. And the orientation of the piece. Orientation
 ;; is changed in conjunction with the pointers to the
 ;; filled in parts.
-(defstruct piece x y id filled-coords orientation)
+(defstruct piece x y id coords orientation)
+
+(defun adjusted-piece (piece overrides)
+  (let ((copy (copy-structure piece)))
+    (loop for (slot-name value) in overrides do
+          (setf (slot-value copy slot-name) value))
+    copy))
 
 (defun piece-down (piece)
-  (make-piece :x (piece-x piece)
-              :y (1+ (piece-y piece))
-              :id (piece-id piece)
-              :filled-coords (piece-filled-coords piece)
-              :orientation (piece-orientation piece)))
+  (adjusted-piece piece `((y ,(1+ (piece-y piece))))))
 
 (defun piece-left (piece)
-  (make-piece :x (1- (piece-x piece))
-              :y (piece-y piece)
-              :id (piece-id piece)
-              :filled-coords (piece-filled-coords piece)
-              :orientation (piece-orientation piece)))
+  (adjusted-piece piece `((x ,(1- (piece-x piece))))))
 
 (defun piece-right (piece)
-  (make-piece :x (1+ (piece-x piece))
-              :y (piece-y piece)
-              :id (piece-id piece)
-              :filled-coords (piece-filled-coords piece)
-              :orientation (piece-orientation piece)))
+  (adjusted-piece piece `((x ,(1+ (piece-x piece))))))
 
 (defun piece-rotate (piece)
-  (multiple-value-bind (new-filled-coords new-orientation)
-      (next-orientation (piece-id piece) (piece-orientation))
-    (make-piece :x (piece-x piece)
-                :y (piece-y piece)
-                :id (piece-id piece)
-                :filled-coords new-filled-coords
-                :orientation new-orientation)))
+  (multiple-value-bind (new-coords new-orientation)
+      (next-orientation (piece-id piece) (piece-orientation piece))
+    (adjusted-piece piece `((coords ,new-coords)
+                        (orientation ,new-orientation)))))
 
-(defconstant piece-bbox-size 4)
+(defparameter piece-moves
+  (list #'piece-left
+        #'piece-right
+        #'piece-down
+        #'piece-rotate))
+
+(defconstant +piece-bbox-size+ 4)
+(defconstant +n-orientations+ 4)
 
 (defparameter piece-orientations
   (make-hash-table :test #'equal))
@@ -66,31 +64,45 @@
     (values next-orientation-index
             (nth next-orientation-index orientations))))
 
-(defun add-piece (template)
-  ;; Create & return constructor function, and
-  ;; also add the piece's orientations to the list.
+(defun generate-piece (template)
+  ;; Generate all of the piece's possible orientations, add
+  ;; them to the map of piece -> orientations.
+  ;; Then return the piece in its first orientation and starting
+  ;; position (bounding box in top left corner).
   (when (not (= +piece-bbox-size+ (length template)))
-    (error "Piece is the wrong size." template))
-  (defparameter filled-coords (list))
-  (loop for y from 0 below +piece-bbox-size+ do
-        (let ((row (nth y template)))
-          (when (not (= +piece-bbox-size+ (length row)))
-            (error "Piece row is wrong size." template))
-          (loop for x from 0 below +piece-bbox-size+ do
-                (when (equal #\# (char row x))
-                  (push (list x y) (car filled-coords))))))
-  ;; TODO do rotations (x=4-y-1, y=x), add to orientations list.
-  ;; then return a constructor.
-  ;; Also... should moving a piece always create a new struct?
-  ;; Seems a bit wasteful.
-  ;; Not sure what the next steps are after that.
-  )
+    (error "Piece is the wrong size."))
+  (defun rotate-coords (n x y)
+    (if (= n 0)
+        (list x y)
+        (rotate-coords (1- n) (- +piece-bbox-size+ y 1) x)))
+  (let ((coords (parse-piece-template template))
+        (piece-id (hash-table-count piece-orientations)))
+    (setf (gethash piece-id piece-orientations)
+          (loop for num-rotations from 0 below +n-orientations+ collect
+                (loop for (x y) in coords collect
+                      (rotate-coords num-rotations x y))))
+    (make-piece
+     :x 0
+     :y 0
+     :id piece-id
+     :coords coords
+     :orientation 0)))
+
+(defun parse-piece-template (template)
+  (apply #'append
+         (loop for y from 0 below +piece-bbox-size+ collect
+               (let ((row (nth y template)))
+                 (when (not (= +piece-bbox-size+ (length row)))
+                   (error "Piece row is wrong size."))
+                 (loop for x from 0 below +piece-bbox-size+
+                       when (equal #\# (char row x))
+                       collect (list x y))))))
 
 ;; Order is important, as described in
 ;; the original HATETRIS code.
-(defconstant +piece-constructors+
+(defparameter +pieces+
   (mapcar
-   #'add-piece
+   #'generate-piece
    ;; Templates.
    (list
     '("...."
@@ -120,11 +132,8 @@
     '("...."
       ".###"
       "..#."
-      "....")
-    )))
+      "...."))))
 ;; things to do:
-;;   1. generate next possible moves given a certain piece.
-;;      1.1. define pieces & their unique rotations.
 ;;      1.2. algorithm to find valid positions (don't copy whole
 ;;           grid yet, have some sort of "intermediate" copy that's
 ;;           just a pair of the well and the piece position... maybe.
