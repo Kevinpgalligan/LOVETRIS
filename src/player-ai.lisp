10c697affba6e7b185405e6ff775f6831a0a759b
kevinpgalligan <galligankevinp@gmail.com>
2020-04-09 23:25:20 +0100

    Implement standard Tetris heuristics, to be used in search algorithms.

--- src/player-ai.lisp
+++ src/player-ai.lisp
@@ -6,3 +6,42 @@
   (let ((options (get-placements state piece)))
     (nth (random (length options))
          options)))
+
+;;;; Returns a heuristic function with the given
+;;;; weights on different characteristics of the
+;;;; game.
+(defun heuristic-evaluator (w-aggregate-height w-score w-holes w-bumpiness)
+  (lambda (state)
+    (+ (* w-aggregate-height (aggregate-height state))
+       (* w-score (state-score state))
+       (* w-holes (num-holes state))
+       (* w-bumpiness (bumpiness state)))))
+
+(defun aggregate-height (state)
+  (loop for x from 0 below (well-width state)
+        sum (column-height state x)))
+
+(defun column-height (state x)
+  (loop for y from 0 below (well-height state)
+        while (equalp +empty+ (get-square state x y))
+        finally (return (- (well-height state) y))))
+
+(defun num-holes (state)
+  (loop for x from 0 below (well-width state) sum
+        (loop for y from 1 below (well-height state) sum
+              ;; Should probably abstract the square types
+              ;; better. Have functions like is-empty-square-p
+              ;; and is-full-square-p.
+              ;; Also, this seems to be a weird definition of
+              ;; a "hole", should it perhaps be surrounded on
+              ;; the sides as well?
+              (if (and (equalp +empty+ (get-square state x y))
+                       (equalp +full+ (get-square state x (- y 1))))
+                  1
+                  0))))
+
+(defun bumpiness (state)
+  (loop for x from 1 below (well-width state) sum
+        ;; Ugh. Computing height of each column twice.
+        (abs (- (column-height state x)
+                (column-height state (- x 1))))))
